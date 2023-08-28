aaa699a72c8319d33f7d95d29e310b93a1ff95fe
scrp <scrappyjs666@gmail.com>
2023-08-29 01:44:40 +0300

    added lodash debounce function

--- client/src/components/Search/index.jsx
+++ client/src/components/Search/index.jsx
@@ -1,14 +1,15 @@
 import { TextField } from '@mui/material';
 import { useDispatch } from 'react-redux';
+import debounce from 'lodash.debounce';
 import { changeInputValue } from '../../store/search/actions';
 
 function Search() {
   const dispatch = useDispatch();
 
-  const handleSearch = (event) => {
+  const handleSearch = debounce((event) => {
     const searchTerm = event.target.value;
     dispatch(changeInputValue(searchTerm));
-  };
+  }, 300);
 
   return <TextField label="Find car..." onChange={handleSearch} />;
 }
